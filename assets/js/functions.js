25555bf69827e93da1c11a58d633a242a1234695
Fabien <fabien.webdev@gmail.com>
2016-10-23 16:04:23 +0900

    Ajax call to the api and basic html structure

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,1 +1,52 @@
+$(document).ready(function() {
+  // Stuff to do as soon as the DOM is ready
 
+  //Variable to store element
+  var $queryMsg = $('p#queryMsg');
+  var $userName  = $('.userName');
+  var $bio  = $('.bio');
+  var $logo  = $('.logo');
+  var $status  = $('.status');
+  var $views  = $('.views');
+  var $followers  = $('.followers');
+  var $game  = $('.game');
+  var $viewers  = $('.viewers');
+  var $preview  = $('.preview');
+
+
+
+
+
+  $.ajax({
+    method: "GET",
+    url: "https://wind-bow.hyperdev.space/twitch-api/streams/superplay",
+    dataType:'jsonp',
+    timeout: 2000,
+    beforeSend: function(){
+      $queryMsg.text("Fetching data, please wait...");
+    },
+    complete: function(){
+      $queryMsg.text('');
+    },
+    success: function(data){
+      console.log(data);
+      
+
+
+
+
+    },
+    error: function(data){
+      $queryMsg.html('<p class="error">An error occured while trying to fetch data. Please try again later.</p>')
+    }
+
+
+
+
+  })
+
+
+
+
+
+});
